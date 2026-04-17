0ebdc6356c609e3cee12ab503953fd882851f4f3
Min <thu0989905368@gmail.com>
2026-04-17 09:36:08 +0700

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,14 @@
+dai=float(input("Nhap chieu dai day hinh chu nhat(cm): "))
+rong=float(input("Nhap chieu rong day hinh chu nhat(cm): "))
+cao=float(input("Nhao chieu cao hinh khoi chu nhat(cm): "))
+sole=int(input("So luong so le can hien thi:"))
+dientichday=dai*rong
+thetichkhoi=dai*rong*cao
+print(f"Dien tich day hinh chu nhat = {dientichday:.{sole}f}cm\u00b2")
+print(f"The tich hinh khoi = {thetichkhoi:.{sole}f}cm\u00b3")
+
+
+
+
+
+#
