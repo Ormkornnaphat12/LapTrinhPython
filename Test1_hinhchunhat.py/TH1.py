769231436c529186a8c4eb6c60c514a19ddd1c64
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-17 10:02:09 +0700

    Bai tap dien tich va the tich hinh chu nhat

---
+++ Test1_hinhchunhat.py/TH1.py
@@ -0,0 +1,28 @@
+dai=float(input("Nhap chieu dai day hinh chu nhat(cm): "))
+rong=float(input("Nhap chieu rong day hinh chu nhat(cm): "))
+cao=float(input("Nhao chieu cao hinh khoi chu nhat(cm): "))
+sole=int(input("So luong so le can hien thi:"))
+dientichday=dai*rong
+thetichkhoi=dai*rong*cao
+print(f"Dien tich day hinh chu nhat = {dientichday:.{sole}f}cm\u00b2")
+print(f"The tich hinh khoi = {thetichkhoi:.{sole}f}cm\u00b3")
+
+
+
+
+
+#dai = input("Nhap chieu dai day hinh chu nhat(cm): ")
+#rong = input("Nhap chieu rong day hinh chu nhat(cm): ")
+#cao = input("Nhao chieu cao hinh khoi chu nhat(cm): ")
+#sole = input("So luong so le can hien thi:")
+#dinhdang = '{:.'+sole+'f}'
+#dai =float(dinhdang.format(eval(dai)))
+#rong=float(dinhdang.format(eval(rong)))
+#cao=float(dinhdang.format(eval(cao))
+#dientich=dai*rong
+#dientich=float(dinhdang.format(dientich))
+#thetich=dai*rong*cao
+#thetich=float(dinhdang.format(thetich))
+#print("Dien tich day hinh chu nhat = ",dientich,"cm\u002")
+#print("The tich hinh khoi = ",thetich"cm\u00b3")*
+
