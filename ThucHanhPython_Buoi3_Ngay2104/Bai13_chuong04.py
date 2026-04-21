0c034f9c251204b1f9df3fb77402ccf286361c8e
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-21 09:30:22 +0700

    Nop bai thuc hanh 06 vaf 13 buoi 3 chuong 04

---
+++ ThucHanhPython_Buoi3_Ngay2104/Bai13_chuong04.py
@@ -0,0 +1,8 @@
+s='''     Quê hương
+    Quê hương là   chùm khế   ngọt.
+        Cho con trèo hái   mỗi  ngày   .
+    Quê  hương là   đường  đi học .
+    Con về rợp bướm vàng bay.
+      Đỗ     Trung Quân    '''
+clean_s=s.replace("  ","").strip()
+print(clean_s)
