081752081f99c14ec081e91b642b0fad96060fac
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-24 10:06:17 +0700

    "Cau a thuc hanh lambda chuong 5"

---
+++ Thuchanh_buoi4_chuong05/cau_a.py
@@ -0,0 +1,6 @@
+#a)Nhan 1 doi so la so nguyen cua n  va tra ve tri tuyet doi cua n
+print('--- a) Nhan 1 doi so la so nguyen cua n  va tra ve tri tuyet doi cua n ---')
+trituyetdoi = lambda n : abs(n)
+chuoi_nhap = input('Nhap vao 1 doi: ')
+so = int(chuoi_nhap)
+print('Trị tuyệt đối là: ',trituyetdoi(so)) 
