24db7687baaa346ce21a3f19c44e50474d8e93ad
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-24 10:08:10 +0700

    "Cau b thuc hanh lambda chuong 05"

---
+++ Thuchanh_buoi4_chuong05/cau_b.py
@@ -0,0 +1,6 @@
+print('--- b) Nhan 1 doi so la so nguyen cua n  va tra ve gia tri cua n+15 ---')
+
+cong_15 = lambda n: n+15
+nhap = input('Nhap vao 1 so: ')
+number = int(nhap)
+print('Gia tri cua n+15 la:',cong_15(number))
