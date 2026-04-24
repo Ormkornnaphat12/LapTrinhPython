07874807b31c87038945ae38f3e69f0cc79a32a4
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-24 10:09:51 +0700

    "Cau e thuc hanh lamdba chuong 5"

---
+++ Thuchanh_buoi4_chuong05/cau_e.py
@@ -0,0 +1,6 @@
+import math
+print('--- e) Nhan 1 doi so la so thuc r la ban kinh cua hinh tron. Cho biet dien tich hinh tron ---')
+Dien_tich = lambda r: math.pi*r**2
+ban_kinh = input('Nhap vao r: ')
+Tinh_r = float(ban_kinh)
+print('Dien tich cua hinh tron la: ',Dien_tich(Tinh_r))
