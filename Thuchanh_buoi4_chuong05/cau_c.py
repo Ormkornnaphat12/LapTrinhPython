6a73c6c78a35b909c46010984961403044a22486
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-24 10:09:00 +0700

    "Cau c thuc hanh lamdba chuong 5"

---
+++ Thuchanh_buoi4_chuong05/cau_c.py
@@ -0,0 +1,7 @@
+print('--- c) Nhan 2 doi so la so nguyen (x,y) va tra ve tich cua x va y ---')
+Tich = lambda x, y: x*y
+nhapx = input('Nhap x = ')
+x = int(nhapx)
+nhapy = input('Nhap y =')
+y = int(nhapy)
+print('Tich cua x va y  = ', Tich(x,y))
